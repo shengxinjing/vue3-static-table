d35b4196ee6c7517ce9ed1dbca08318b8cad7c11
shengxinjing <316783812@qq.com>
2020-05-31 13:45:25 +0800

    show static table display bug

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<table>
+  <tr>
+    <td class="col-md-1"> vue3 </td>
+    <td class="col-md-1"> vue3 </td>
+    <td class="col-md-1"> vue3 </td>
+    <td class="col-md-1"> vue3 </td>
+    <td class="col-md-1"> vue3 </td>
+  </tr>
+</table>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.col-md-1{
+  border:1px solid black;
+}
+</style>
